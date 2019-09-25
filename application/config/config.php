b7d57ca725b829ffbe9f5b1dd6fd1e1619bd7ee6
ferry <ferry_ts@ugm.ac.id>
2019-09-25 20:50:25 +0700

    ubah config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://kpi.local/';
+//$config['base_url'] = 'http://kpi.local/';
+$config['base_url'] = (!empty($_SERVER['HTTPS_ENV']) && $_SERVER['HTTPS_ENV'] !== 'off') ? "https" : "http";
+$config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
+$config['base_url'] .= preg_replace('@/+$@','',dirname($_SERVER['SCRIPT_NAME'])).'/';
+
 
 /*
 |--------------------------------------------------------------------------
