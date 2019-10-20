a35743c69536963a325bc314a12abe0e380d6fc3
regha <rega.blank@gmail.com>
2019-10-20 07:27:51 +0700

    [rega] update password fitur

--- application/modules/kpi/controllers/Profile.php
+++ application/modules/kpi/controllers/Profile.php
@@ -61,18 +61,63 @@ class Profile extends MY_Controller
 
 		//update
 		$additional_data['id'] = $id_user;
-		if (!$this->ion_auth->is_admin() && $user->active_admin==0 ) {
+		if (!$this->ion_auth->is_admin() && $user->active_admin == 0) {
 			//set active admin 2 -> pending
 			$additional_data['active_admin'] = 2;
 		}
 		// var_dump($additional_data);
-		$id = $this->users_model->from_form(NULL, $additional_data)->update(null,['id'=>$id_user]);
+		$id = $this->users_model->from_form(NULL, $additional_data)->update(null, ['id' => $id_user]);
 		// var_dump($id);
 		if ($id) {
 			$this->session->set_flashdata('success', 'Data periode berhasil diupdate');
 			redirect('/kpi/profile');
 		} else {
 			$this->index();
+		}
+	}
+
+
+
+
+	public function save_password()
+	{
+		$post = $this->input->post();
+		// var_dump($post);
+		$password = $post['password'];
+		$user = $this->ion_auth->user()->row();
+		// var_dump($user->password);
+		$id = $user->id;
+		$originalPassword = $this->input->post('current_password');
+
+		if ($this->ion_auth->verify_password($originalPassword, $user->password) == TRUE) {
+			//shall pass
+			// echo "true";
+			//check password validation
+			$this->form_validation->set_rules('password', 'Password', "min_length[6]|regex_match[/[A-Za-z]/]", [
+				'min_length' => 'Minimal Password harus 6 karakter',
+				'regex_match' => 'Password harus memiliki huruf',
+				'numeric' => 'Password harus memiliki angka',
+			]);
+			if ($this->form_validation->run() == TRUE) {
+				// var_dump("true");
+				$data = array(
+					'password' => $password,
+				);
+				$ok = $this->ion_auth->update($id, $data);
+				if ($ok) {
+					$this->session->set_flashdata('success', 'Password berhasil diupdate');
+				} else {
+					$this->session->set_flashdata('error', 'Password gagal diupdate, silahkan coba lagi');
+				}
+				redirect(base_url() . '/kpi/profile');
+				//update password
+			} else {
+				$this->index();
+				// echo "false";
+			}
+		} else {
+			$this->session->set_flashdata('warning', 'Password sekarang salah');
+			redirect(base_url() . '/kpi/profile');
 		}
 	}
 }
